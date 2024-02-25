4329ac8dbe9119ac79857a1ccb79bb8b5f5c7193
foriver4725 <tk230001@tks.iput.ac.jp>
2024-02-25 09:09:23 +0900

    1st commit

---
+++ Assets/Scripts/OnPrefabs/IsAnimalLookable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// シーンカメラは何も見てはいけない
+public class IsAnimalLookable : MonoBehaviour
+{
+    public bool isLookable { get; set; } = false; // カメラに映っているかどうか
+
+    // このAnimalがゲームカメラに映っている時（障害物関係無し）だけ，フラグをオンにする。
+    private void OnBecameVisible()
+    {
+        isLookable = true;
+    }
+    private void OnBecameInvisible()
+    {
+        isLookable = false;
+    }
+}
